050ec26bf03dddf450bb407e8688bfbb7bf13e48
miyukataoka <34929776+miyukataoka@users.noreply.github.com>
2018-01-10 20:13:50 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
-console.log("Up and running!");
-
-//Use console.log() to show two cards that are flipped
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-
 // Create array to store all of the cards
 var cards = ["queen", "queen", "king", "king"];
 
 // Create array to store the cards in play;
 var cardsInPlay = []
 
-//Create a variable cardOne to represent first card that is flipped
-var cardOne = cards[0];
-
-// Add card to array of cards that are in play
-cardsInPlay.push(cardOne);
-
-// Use console.log() to display the card that is flipped
-console.log("User flipped" + cardOne);
-
-// Create variable cardTwo for second card that is flipped
-var cardTwo = cards[2];
-
-// Add card to array of cards that are in play
-cardsInPlay.push(cardTwo);
-
-//Use console.log() to shoow card that is flipped
-console.log("User flipped " + cardTwo);
-
-// Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay [0] === cardsInPlay [1]) {
-    alert("You found a match!");
+// Create function to check if there is a match
+var checkForMatch = function () {
+// Check to see if two cards match and give response
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
   }
+};
+
+// Create a functon to represent user flipping card
+var flipCard = function (cardId) {
+// Show card that user just flipped
+  console.log("User flipped " + cards[cardId]);
+
+// Add card to array of cards that are in play
+cardsInPlay.push(cards[cardId]);
+
+// Check if two cards have been played
+if (cardsInPlay.length === 2) {
+  checkForMatch();
 }
+};
+// Calls flipCard after it has been defined
+flipCard(0);
+flipCard(2);
